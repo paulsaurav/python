7cd7ce5603053ffbfededdc59d524e9b3fa57797
Saurav Paul <72635245+paulsaurav@users.noreply.github.com>
2023-08-24 18:25:58 +0000

    10

--- stringFormatting.py
+++ stringFormatting.py
@@ -1,2 +1,12 @@
+
 # string formatting
 print("Hello {0} {1}".format("Saurav", True))
+followers = 500
+following = 153
+posts = 86
+print("Followers {0} | Following {1} | Posts {2}".format(followers, following, posts))
+
+followers *= 2
+
+print("Followers {0} | Following {1} | Posts {2}".format(followers, following, posts))
+

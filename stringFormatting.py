df64da3181ed70857c7b9ef63885f135c554e635
Saurav Paul <72635245+paulsaurav@users.noreply.github.com>
2023-08-24 18:33:55 +0000

    Strings

--- stringFormatting.py
+++ stringFormatting.py
@@ -1,5 +1,6 @@
 
 # string formatting
+#String manipulation 
 print("Hello {0} {1}".format("Saurav", True))
 followers = 500
 following = 153
@@ -10,3 +11,14 @@ followers *= 2
 
 print("Followers {0} | Following {1} | Posts {2}".format(followers, following, posts))
 
+
+
+myName = "Paul"
+myName = "Saurav"
+myName = myName + " " + "Paul"
+print(myName)
+myName += " King"
+print(myName)
+
+x = "x"
+print(x * 100)
